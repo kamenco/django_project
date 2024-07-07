4be5653fa0b205b92986bcf8db8c91697289bf17
Kamen Kirov <kamen@bgtown.com>
2024-07-07 18:46:58 +0000

    Edited file urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,6 +20,6 @@ from hello_world import views as index_views
 
 
 urlpatterns = [
-    path('', index_views.index, name='index')
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
